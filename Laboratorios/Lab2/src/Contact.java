e0dbd4deb0ca3f9583cafe84924f7710ea3a75b7
cris7ian3 <cristrojas1e@email>
2024-09-09 01:03:51 -0400

    Mi Laboratorio 2 de Tecnicas de Programacion 2 Segundo Semestre

---
+++ Laboratorios/Lab2/src/Contact.java
@@ -0,0 +1,51 @@
+public class Contact {
+    private String name;
+    private String email;
+    private int age;
+    private String phone;
+
+    public Contact(String name, String email, int age, String phone) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Email: " + email + ", Age: " + age + ", Phone: " + phone;
+    }
+}
